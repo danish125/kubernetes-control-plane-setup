b1fe0faa7df8f6cd5bb78a2e6da9e435d3157051
danish125 <drehman073@gmail.com>
2025-07-15 01:17:05 +0100

    codebuild custom image

--- aws_codebuild_github_runner/extras/push-image.sh
+++ aws_codebuild_github_runner/extras/push-image.sh
@@ -1,4 +1,5 @@
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 339713106964.dkr.ecr.eu-west-2.amazonaws.com
 docker buildx build --platform linux/amd64 -t terraform-image . --load
+# docker build -t terraform-image .
 docker tag terraform-image:latest 339713106964.dkr.ecr.eu-west-2.amazonaws.com/codebuild-custom-image:latest
 docker push 339713106964.dkr.ecr.eu-west-2.amazonaws.com/codebuild-custom-image:latest
